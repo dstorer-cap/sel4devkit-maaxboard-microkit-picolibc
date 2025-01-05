8efd287b70a1517b037f127a78c5bc059dc36824
dstorer-cap <daniel.storer@capgemini.com>
2025-01-05 23:05:25 +0000

    Added build script for none-elf

--- build_script.sh
+++ build_script.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-sudo rm -rf picolibc-microkit/
-sudo mkdir picolibc-microkit
-cd picolibc-microkit
+export PATH=/home/dstorer/SP800-90B_EntropyAssessment/gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf/bin:$PATH
+rm -rf none-elf-build/
+mkdir none-elf-build
 mkdir ../../picolibc_build
-sudo ../scripts/do-aarch64-configure-nocrt -Dprefix=${PWD}/../../picolibc_build
-sudo ninja 
-sudo ninja install
+meson setup none-elf-build \
+    --cross-file scripts/cross-aarch64-none-elf.txt \
+    --prefix=${PWD}/../../picolibc_build \
+    -Dnewlib-global-errno=true \
+    -Dnewlib-reent-small=true \
+    -Dnewlib-io-long-long=false 
+ninja -C none-elf-build
+ninja -C none-elf-build install
